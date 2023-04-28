bfaeff18b90f57a6411a4a93f226668cddd5a6a8
jeanmichael1993 <jeanmichael760@gmail.com>
2023-04-28 11:59:19 -0300

    Adicionado atividades 9 e 10 e suas respostas

---
+++ Seção03/Ex01-Ex10/Ex09.py
@@ -0,0 +1,6 @@
+qtd: int = int(input("Digite um número inteiro: "))
+for x in range(qtd):
+    if x % 2 == 1:
+        print(x)
+
+        
---
+++ Seção03/Ex01-Ex10/Ex10.py
@@ -0,0 +1,6 @@
+soma: int = 0
+for x in range(50):
+    if x % 2 == 0:
+        print(x)
+        soma += x
+print(soma)
