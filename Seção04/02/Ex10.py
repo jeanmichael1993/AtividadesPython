4b3851f78dd8851d9597cf997b196720196f3a91
Jean Michael <jeanmichael760@gmail.com>
2024-01-05 13:34:51 -0300

    adicionado respostas atividades 8,9 e 10

---
+++ Seção04/02/Ex10.py
@@ -0,0 +1,20 @@
+matriz: int = [[1*0 for i in range(1, 4)] for j in range(1, 4)]
+soma: int = 1
+
+"""
+guardar dados 1 a 9 na matriz
+"""
+for i in range(3):
+    for j in range(3):
+        matriz[i][j] = soma
+        soma += 1
+"""
+somar numeros da diagonal principal
+"""
+soma_2: int = 0
+for i in range(3):
+    for j in range(3):
+        if i == j:
+            soma_2 += (matriz[i][j])
+print(soma_2)
+
