cf40d36bd9b9d99d5a80ae4128102b10cbc434e8
Jean Michael <jeanmichael760@gmail.com>
2024-01-19 18:18:21 -0300

    atualizado ex10

--- Seção05/Ex10.py
+++ Seção05/Ex10.py
@@ -4,9 +4,13 @@ def num_maior(num_a: int, num_b: int) -> int:
     else:
         return num_b
 
-
-if __name__ == '__main__':
+def guardar_dados() -> int:
     num_a: int = int(input("Digite o valor de A: "))
     num_b: int = int(input("Digite o valor de B: "))
+    return num_a, num_b
+
+
+if __name__ == '__main__':
+    num_a, num_b = guardar_dados()
     print(f'o valor maior entre A e B é o : {num_maior(num_a, num_b)}')
 
