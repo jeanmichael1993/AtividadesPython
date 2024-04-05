62a6c9aff0dae7bf56c22b39701b57afdcb98571
Jean Michael <jeanmichael760@gmail.com>
2024-04-05 13:39:23 -0300

    adicionado resposta atividade 03

--- Seção07/Ex03.py
+++ Seção07/Ex03.py
@@ -1,7 +1,57 @@
-pessoas: str = [['jean',18,1.80],['teste1',20, 1.75],['teste2',20,1.79],['teste3', 30, 1.67]]
 
-for i in pessoas:
-    if i[0].upper() == 'JEAN':
-        del pessoas[pessoas.index(i)]
+class Elevator:
 
-print(pessoas)
+
+    def __int__(self):
+        self.__andar = 0
+        self.__total_andar = 0
+        self.__cap_elevator = 0
+        self.__pessoas = 0
+
+    def inicializa(self, cap_elevator, total_andares):
+        self.__cap_elevator = cap_elevator
+        self.__total_andar = total_andares
+        self.__andar = 0
+        self.__pessoas = 0
+
+
+    def entra(self):
+        if self.__cap_elevator > self.__pessoas:
+            self.__pessoas += 1
+            return print(f"Tem {self.__pessoas} no elevador.")
+
+    def sai(self):
+        if self.__pessoas > 0:
+            self.__pessoas -= 1
+            return print(f"Tem {self.__pessoas} no elevador.")
+
+    def sobe(self):
+        if self.__andar < self.__total_andar:
+            self.__andar += 1
+            return print(f"Andar atual: {self.__andar}")
+
+    def desce(self):
+        if self.__andar >= 1:
+            self.__andar -= 1
+            return print(f"Andar atual: {self.__andar}")
+
+
+def main():
+    elevator = Elevator()
+    elevator.inicializa(10, 5)
+    elevator.sobe()
+    elevator.sobe()
+    elevator.entra()
+    elevator.entra()
+    elevator.sai()
+    elevator.sai()
+    elevator.sai()
+    elevator.desce()
+    elevator.desce()
+    elevator.desce()
+
+
+if __name__ == "__main__":
+    main()
+
+
