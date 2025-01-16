5989e3eaf519aa092a04421503bb1254de7c3e27
jeanmichael1993 <jeanmichael760@gmail.com>
2025-01-16 13:26:14 -0300

    Added activity 1187 beecrowd

---
+++ Activity_beecrowd/beecrowd_1187.py
@@ -0,0 +1,40 @@
+"""
+Read an uppercase character, that indicates an operation to be performed and a matrix[12][12].
+Then calculate and display the sum and the average considering only those elements that are upper area of the matrix.
+
+Input()
+The first line of input contains a single uppercase character ("S","M"), indicates an operation (SUM or AVERAGE)
+to be performed on the elements of matrix.
+
+Output()
+Print the requested result (sum or average) with one digit after the decimal point.
+
+"""
+
+def create_matrix(lines: int, columns: int) -> list:
+    return [[float(input()) for column in range(columns)] for line in range(lines)]
+
+
+def operation_matrix(lines: int, columns: int, operation: str, matrix: list):
+    __sum__: float = 0
+    __list__: list = []
+
+    for line in range(5,0,-1):
+        for column in range(12 - line, line,-1):
+            __sum__ += matrix[line][column]
+            __list__.append(matrix[line][column])
+
+    if operation == "S":
+        print(f"{__sum__:.1f}")
+    elif operation == "M":
+        print(f"{(__sum__ / len(__list__)):.1f}")
+
+
+def main():
+    lines = columns = 12
+    operation = input()
+    operation_matrix(lines,columns,operation,create_matrix(lines,columns))
+
+
+if __name__ == "__main__":
+    main()
